915aa081271bcd10203570faddf7b9c4d122086f
MINDQ <MINDQ@MINDQ_001>
2023-08-14 12:49:35 +0530

    FirstProject

---
+++ DCBBank_GoldLoan/src/test/java/p1/TestA.java
@@ -0,0 +1,25 @@
+package p1;
+
+import org.testng.annotations.Test;
+
+public class TestA 
+{
+  @Test
+  public void tc001() 
+  {
+	  System.out.println("Tc001 Executed");
+  }
+  
+  @Test
+  public void tc002() 
+  {
+	  System.out.println("Tc002 Executed");
+  }
+
+
+  @Test
+  public void login() 
+  {
+	  System.out.println("login  Executed");
+  }
+}
