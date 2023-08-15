c3272669a029ee27a051fe08a09327e1a7c5c090
MINDQ <MINDQ@MINDQ_001>
2023-08-15 12:24:19 +0530

    Welcome Msg printed

---
+++ src/test/java/p4/TestE.java
@@ -0,0 +1,11 @@
+package p4;
+
+public class TestE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome");
+		System.out.println("new bracnhc");
+	}
+
+}
